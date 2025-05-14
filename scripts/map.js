d5cc9cdd1ea7fae87d687c33852611bd4a983bd7
Mathurin Chritin <mathurin.chritin@epfl.ch>
2025-05-14 13:50:37 +0200

    Updates

--- scripts/map.js
+++
@@ -1,6 +0,0 @@
-export function initMap() {
-    const map = L.map("map_eur").setView([50, 10], 4);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "&copy; OpenStreetMap contributors"
-    }).addTo(map);
-}
